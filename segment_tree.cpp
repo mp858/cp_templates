3f3f214f44f9dc710e28dc65b8ef3f068f90cd80
mp858 <108896435+mp858@users.noreply.github.com>
2024-09-24 16:52:47 +0530

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,33 +1,70 @@
-vector<long long> t(4*200010),v;  //size if t should be 4*n
+#include<bits/stdc++.h>
 
-void build(int n,int s,int e){
-  if(s==e){
-    t[n]=v[s];
-    return;
+//structure of each node of segment tree
+struct Node {
+  long long minVal;
+  Node() {
+    minVal = 1e9;
   }
-  long long m=s+(e-s)/2;
-  build(2*n,s,m);
-  build(2*n+1,m+1,e);
-  t[n]=t[2*n]+t[2*n+1];
-}
-void update(long long n,long long s,long long e,long long i,long long x){
-  if(s==e){
-    t[n]=x;
-    return;
+  Node(long long val) {
+    minVal = val;
   }
-  long long m=(s+e)/2;
-  if(i<=m)
-  update(2*n,s,m,i,x);
-  else
-  update(2*n+1,m+1,e,i,x);
-  t[n]=t[2*n]+t[2*n+1];
-  return;
+};
+
+Node merge(Node a, Node b) {
+  Node res;
+  res.minVal = min(a.minVal, b.minVal);
+  return res;
 }
-long long query(long long n,long long s,long long e,long long ql,long long qr){
-  if(s>=ql&&e<=qr)
-  return t[n];
-  if(s>qr||e<ql)
-  return 0;
-  long long m=s+(e-s)/2;
-  return (query(2*n,s,m,ql,qr)+query(2*n+1,m+1,e,ql,qr));
-}
+
+class SegmentTree {
+private:
+  vector<Node> t;
+  long long n;
+  void build(vector<long long> &arr, long long id, long long l, long long r) {
+    if(l == r) {
+      t[id] = Node(arr[l]);
+      return;
+    }
+    long long mid = (l + r) / 2;
+    build(arr, id << 1, l, mid);
+    build(arr, id << 1 | 1, mid + 1, r);
+    t[id] = merge(t[id << 1], t[id << 1 | 1]);
+  }
+  void update(long long id, long long l, long long r, long long ind, long long val) {
+    if(ind < l || ind > r) {
+      return;
+    }
+    if(l == r) {
+      t[id] = Node(val);
+      return;
+    }
+    long long mid = (l + r) >> 1;
+    update(id << 1, l, mid, ind, val);
+    update(id << 1 | 1, mid + 1, r, ind, val);
+    t[id] = merge(t[id << 1], t[id << 1 | 1]);
+  }
+  Node query(long long id, long long l, long long r, long long lq, long long rq) {
+    if(l > rq || lq > r) {
+      return Node();
+    }
+    if(lq <= l && r <= rq) {
+      return t[id];
+    }
+    long long mid = (l + r) >> 1;
+    Node res = merge(query(id << 1, l, mid, lq, rq), query(id << 1 | 1, mid + 1, r, lq, rq));
+    return res;
+  }
+public:
+  SegmentTree(vector<long long> &arr) {
+    n = arr.size();
+    t.resize(4 * n);
+    build(arr, 1, 0, n - 1);
+  }
+  void update(long long ind, long long val) {
+    update(1, 0, n - 1, ind, val);
+  }
+  Node query(long long lq, long long rq) {
+    return query(1, 0, n - 1, lq, rq);
+  }
+};
